264da9238be4ee0fdbe5b4452ed34fbf9bdb1c40
ZOOO-max <jo@fujiijounoMacBook-Air.local>
2025-08-09 17:38:41 +0900

    first commit

--- main.py
+++ main.py
@@ -4,4 +4,11 @@ git commit -m "first342 commit"
 git branch -M main
 git remote add origin https://github.com/Z0O0-max/my-first-project.git
 git push -u origin main
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Z0O0-max/my-first-project.git
+git push -u origin main
+
 
